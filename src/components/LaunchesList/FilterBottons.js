7435834fad782fefa013fde20a1300c5a5cb6da7
sklalaludek <pattiegolis@gmail.com>
2018-06-17 21:17:59 +0200

    Add styled btns subtree commit

--- src/components/LaunchesList/FilterBottons.js
+++ src/components/LaunchesList/FilterBottons.js
@@ -4,12 +4,13 @@ const FilterBottons = (props) => {
   const { options } = props;
   return (
     <div className="filter-buttons-container">
-      <button onClick={props.onClick}>All rockets</button>
+      <button onClick={props.onClick} className="filter-buttons-container__btn">All rockets</button>
       {options.map((el, i) => (
         <button
           key={i}
           onClick={props.onClick}
           value={el}
+          className="filter-buttons-container__btn"
         >
           {el}
         </button>
